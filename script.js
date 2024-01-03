700e87cc0bee926200337cbbbf5bdd905a1b7a25
jes5835 <eunsung5835@gmail.com>
2024-01-03 17:27:27 +0900

    t2

---
+++ script.js
@@ -0,0 +1,47 @@
+//$('.js-click-modal').click(function(){
+ //   $('.container').addClass('modal-open');
+ // });
+  
+  //$('.js-close-modal').click(function(){
+  //  $('.container').removeClass('modal-open');
+  //});
+
+
+
+
+  // .play 버튼을 클릭할 때 팝업을 표시합니다.
+//document.querySelector(".play").addEventListener("click", function () {
+ // document.querySelector(".popup").style.display = "flex";
+//});
+
+// .detail 버튼을 클릭할 때 팝업을 닫습니다.
+//document.querySelector(".detail").addEventListener("click", function () {
+ // document.querySelector(".popup").style.display = "none";
+//});
+
+
+//$('.play').click(function(){
+//  $('.popup').addClass('open');
+//});
+
+
+//$('.popup').click(function(){
+ // $(this).removeClass('open');
+//);
+
+
+
+///////////////////////////
+//const popupVideo = document.getElementById('popupVideo');
+
+//function openPopup() {
+//  document.getElementById('videoPopup').style.display = 'block';
+//  popupVideo.controls = true; // controls 속성을 추가하여 컨트롤 바를 표시합니다.
+//}
+
+//function closePopup() {
+//  document.getElementById('videoPopup').style.display = 'none';
+//  popupVideo.controls = false; // controls 속성을 제거하여 컨트롤 바를 숨깁니다.
+//}
+////////////////////////
+
